64b7eeff27196691c120cafbe452dae2ad36bccc
t3nt <44480063+prometheus1337@users.noreply.github.com>
2019-09-07 11:54:21 +0100

    fix lint issues

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+  
 
 from bluetooth.ble import *
 def vibrate(address, lvl):
         sex = "0000fff0-0000-1000-8000-00805f9b34fb", "6e400001-b5a3-f393-e0a9-e50e24dcca9e" # first and second gen toys
         haha = find_service(uuid = services)
-	lol = bluetooth.BluetoothSocket(RFCOMM)
-	lol.connect(address)
+        lol = bluetooth.BluetoothSocket(RFCOMM)
+        lol.connect(address)
 lol.send("Vibrate:" + lvl + ";")
 # todo: add more
