9e9102d1f2ea953f57d5318ef5a7cb1f7657724b
cth103 <cthdev@email.com>
2019-10-22 23:50:58 +0100

    ???

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-  
-
 from bluetooth.ble import *
 def vibrate(address, lvl):
-        sex = "0000fff0-0000-1000-8000-00805f9b34fb", "6e400001-b5a3-f393-e0a9-e50e24dcca9e" # first and second gen toys
-        haha = find_service(uuid = services)
-        lol = bluetooth.BluetoothSocket(RFCOMM)
-        lol.connect(address)
-lol.send("Vibrate:" + lvl + ";")
+        addresses = "0000fff0-0000-1000-8000-00805f9b34fb", "6e400001-b5a3-f393-e0a9-e50e24dcca9e" # first and second gen toys
+        con = find_service(uuid=addresses)
+        sock = bluetooth.BluetoothSocket(RFCOMM)
+        sock.connect(address)
+        sock.send("Vibrate:" + lvl + ";")
+        
 # todo: add more
